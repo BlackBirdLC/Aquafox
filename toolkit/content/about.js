b37593aabca1e22e47e60658a6994be65a9766d9
wireless <thewireless@netcourrier.com>
2024-07-24 18:16:17 +0200

    Ready for beta

--- toolkit/content/about.js
+++ toolkit/content/about.js
@@ -26,7 +26,7 @@ var version = document.getElementById("version");
 
 // paranoia
 if (1 == 1) {
-  version.textContent = "dev";
+  version.textContent = "1.0.0-beta1";
 } else if (versionNum.substr(0,3) == "45.") {
   // FPR series
   var vf = 0 + versionNum.substr(3);
